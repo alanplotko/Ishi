622ccfe0aa36037ea9913696a1dfa51935b10051
Alan Plotko <alan.plotko@gmail.com>
2015-03-22 03:29:04 -0400

    Add window

--- src/app_message.c
+++ src/app_message.c
@@ -20,6 +20,7 @@
   
 static Window *s_main_window;
 static Window *s_question_window;
+static Window *s_ease_window;
 
 static MenuLayer *s_menu_layer, *s_ease_menu_layer;
 static TextLayer *s_text_layer, *s_question_text_layer;
@@ -30,6 +31,20 @@ static int num_menu_items = 0;
 static GBitmap *s_menu_icon_image;
 
 static int s_study_stage = 0;
+
+static void send(int key, int message) {
+  DictionaryIterator *iter;
+  app_message_outbox_begin(&iter);
+  dict_write_int(iter, key, &message, sizeof(int), true);
+  app_message_outbox_send();
+}
+
+static void sendDeckName(int key, const char *message) {
+  DictionaryIterator *iter;
+  app_message_outbox_begin(&iter);
+  dict_write_cstring(iter, key, message);
+  app_message_outbox_send();
+}
 
 /******************************* Build menu **********************************/
 
@@ -62,92 +77,10 @@ static void destroy_menu_titles() {
 	}
 }
 
-/******************************* AppMessage ***********************************/
-
-static void send(int key, int message) {
-  DictionaryIterator *iter;
-  app_message_outbox_begin(&iter);
-  dict_write_int(iter, key, &message, sizeof(int), true);
-  app_message_outbox_send();
-}
-
-static void sendDeckName(int key, const char *message) {
-  DictionaryIterator *iter;
-  app_message_outbox_begin(&iter);
-  dict_write_cstring(iter, key, message);
-  app_message_outbox_send();
-}
-
-static void inbox_received_handler(DictionaryIterator *iterator, void *context) {  
-  Window *window = (Window *)context;
-  Layer *window_layer = window_get_root_layer(window);
-  // Get the first pair
-  Tuple *t = dict_read_first(iterator);
-  // Process all pairs present
-  while(t != NULL) {
-    // Process this pair's key
-    switch(t->key) {
-      // Build menu
-      case KEY_DECKS:
-		    load_menu_titles(t->value->cstring);
-		    layer_mark_dirty(menu_layer_get_layer(s_menu_layer));
-        break;
-      // Question or answer
-      case KEY_QUESTION:
-      case KEY_ANSWER:
-        text_layer_set_text(s_question_text_layer, t->value->cstring);
-		layer_mark_dirty(text_layer_get_layer(s_question_text_layer));
-        break;
-      case KEY_EASE:
-        s_ease = t->value->uint32;
-		layer_add_child(window_layer, menu_layer_get_layer(s_ease_menu_layer));
-		menu_layer_set_click_config_onto_window(s_ease_menu_layer, window);
-        //layer_mark_dirty(menu_layer_get_layer(s_ease_menu_layer));
-        break;
-      default:
-        APP_LOG(APP_LOG_LEVEL_INFO, "Unknown key: %d", (int)t->key);
-        break;
-    }
-
-    // Get next pair, if any
-    t = dict_read_next(iterator);
-  }
-}
-
-static void inbox_dropped_handler(AppMessageResult reason, void *context) {
-  APP_LOG(APP_LOG_LEVEL_ERROR, "Message dropped!");
-}
-
-static void outbox_failed_handler(DictionaryIterator *iterator, AppMessageResult reason, void *context) {
-  APP_LOG(APP_LOG_LEVEL_ERROR, "Outbox send failed!");
-}
-
-static void outbox_sent_handler(DictionaryIterator *iterator, void *context) {
-  APP_LOG(APP_LOG_LEVEL_INFO, "Outbox send success!");
-}
-
-void select_single_click_handler(ClickRecognizerRef recognizer, void *context) {
-  switch(s_study_stage) {
-    case 0:
-      send(KEY_ACTION, ACTION_ANS);
-      s_study_stage++;
-      break;
-    case 1:
-      send(KEY_ACTION, ACTION_EASE);
-      s_study_stage = 0;
-      break;
-  }
-}
-
-void config_provider(Window *window) {
-  // Single click select button
-  window_single_click_subscribe(BUTTON_ID_SELECT, select_single_click_handler);
-}
-
-/******************************* question_window **********************************/
+/******************************* ease_window ***********************************/
 
 static void ease_menu_select_callback(MenuLayer *menu_layer, MenuIndex *cell_index, void *data) {
-   if (cell_index->row == 0){
+   if(cell_index->row == 0) {
      send(KEY_EASE, 0);
      return;
    }
@@ -177,8 +110,6 @@ static void ease_menu_select_callback(MenuLayer *menu_layer, MenuIndex *cell_ind
       } 
       break;
    }
-   layer_remove_from_parent(menu_layer_get_layer(s_ease_menu_layer));
-   window_set_click_config_provider(s_question_window, (ClickConfigProvider) config_provider);
 }
 
 static uint16_t ease_menu_get_num_rows_callback(MenuLayer *menu_layer, uint16_t section_index, void *data) {
@@ -219,19 +150,12 @@ static void ease_menu_draw_row_callback(GContext* ctx, const Layer *cell_layer, 
   menu_layer_reload_data(s_ease_menu_layer);
 }
 
-static void question_window_load(Window *window) {
-  //send(KEY_ACTION, ACTION_Q);
-  // Create text layer with question text
+static void s_ease_window_load(Window *window) {
   Layer *window_layer = window_get_root_layer(window);
-  s_question_text_layer = text_layer_create(GRect(0, 0, 144, 168));
-  text_layer_set_font(s_question_text_layer, fonts_get_system_font(FONT_KEY_GOTHIC_24_BOLD));
-  text_layer_set_text(s_question_text_layer, "Loading Question...");
-  text_layer_set_text_alignment(s_question_text_layer, GTextAlignmentCenter); 
-  layer_add_child(window_layer, text_layer_get_layer(s_question_text_layer));
   
   // Now we prepare to initialize the menu layer
   GRect bounds = layer_get_frame(window_layer);
-  
+
   // Create the menu layer
   s_ease_menu_layer = menu_layer_create(bounds);
   menu_layer_set_callbacks(s_ease_menu_layer, NULL, (MenuLayerCallbacks){
@@ -244,7 +168,80 @@ static void question_window_load(Window *window) {
   });
 
   // Bind the menu layer's click config provider to the window for interactivity
-  //menu_layer_set_click_config_onto_window(s_ease_menu_layer, window);
+  menu_layer_set_click_config_onto_window(s_ease_menu_layer, window);
+  layer_add_child(window_layer, menu_layer_get_layer(s_ease_menu_layer));
+}
+
+static void s_ease_window_unload(Window *window) {
+  menu_layer_destroy(s_ease_menu_layer);
+}
+
+/******************************* AppMessage ***********************************/
+
+static void inbox_received_handler(DictionaryIterator *iterator, void *context) {  
+  Window *window = (Window *)context;
+  Layer *window_layer = window_get_root_layer(window);
+  // Get the first pair
+  Tuple *t = dict_read_first(iterator);
+  // Process all pairs present
+  while(t != NULL) {
+    // Process this pair's key
+    switch(t->key) {
+      // Build menu
+      case KEY_DECKS:
+		    load_menu_titles(t->value->cstring);
+		    layer_mark_dirty(menu_layer_get_layer(s_menu_layer));
+        break;
+      // Question or answer
+      case KEY_QUESTION:
+		    layer_remove_child_layers(window_layer);
+		    layer_add_child(window_layer, text_layer_get_layer(s_question_text_layer));
+      case KEY_ANSWER:
+        text_layer_set_text(s_question_text_layer, t->value->cstring);
+		    layer_mark_dirty(text_layer_get_layer(s_question_text_layer));
+        break;
+      case KEY_EASE:
+        s_ease = t->value->uint32;
+        s_ease_window = window_create();
+        window_set_window_handlers(s_ease_window, (WindowHandlers) {
+          .load = s_ease_window_load,
+          .unload = s_ease_window_unload,
+        });
+        window_stack_push(s_ease_window, true);
+        break;
+      default:
+        APP_LOG(APP_LOG_LEVEL_INFO, "Unknown key: %d", (int)t->key);
+        break;
+    }
+
+    // Get next pair, if any
+    t = dict_read_next(iterator);
+  }
+}
+
+static void inbox_dropped_handler(AppMessageResult reason, void *context) {
+  APP_LOG(APP_LOG_LEVEL_ERROR, "Message dropped!");
+}
+
+static void outbox_failed_handler(DictionaryIterator *iterator, AppMessageResult reason, void *context) {
+  APP_LOG(APP_LOG_LEVEL_ERROR, "Outbox send failed!");
+}
+
+static void outbox_sent_handler(DictionaryIterator *iterator, void *context) {
+  APP_LOG(APP_LOG_LEVEL_INFO, "Outbox send success!");
+}
+
+/******************************* question_window **********************************/
+
+static void question_window_load(Window *window) {
+  send(KEY_ACTION, ACTION_Q);
+  // Create text layer with question text
+  Layer *window_layer = window_get_root_layer(window);
+  s_question_text_layer = text_layer_create(GRect(0, 0, 144, 168));
+  text_layer_set_font(s_question_text_layer, fonts_get_system_font(FONT_KEY_GOTHIC_24_BOLD));
+  text_layer_set_text(s_question_text_layer, "Loading Question...");
+  text_layer_set_text_alignment(s_question_text_layer, GTextAlignmentCenter); 
+  layer_add_child(window_layer, text_layer_get_layer(s_question_text_layer));
 }
 
 static void question_window_unload(Window *window) {
@@ -253,7 +250,27 @@ static void question_window_unload(Window *window) {
 
 /********************************* Buttons ************************************/
 
+void select_single_click_handler(ClickRecognizerRef recognizer, void *context) {
+  switch(s_study_stage) {
+    case 0:
+      send(KEY_ACTION, ACTION_ANS);
+      s_study_stage++;
+      break;
+    case 1:
+      send(KEY_ACTION, ACTION_EASE);
+      s_study_stage++;
+      break;
+    case 2:
+      send(KEY_ACTION, ACTION_Q);
+      s_study_stage = 0;
+      break;
+  }
+}
 
+void config_provider(Window *window) {
+  // Single click select button
+  window_single_click_subscribe(BUTTON_ID_SELECT, select_single_click_handler);
+}
 
 static void menu_select_callback(MenuLayer *menu_layer, MenuIndex *cell_index, void *data) {
   // Use the row to specify which item will receive the select action
